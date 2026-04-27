f5f15128847a0d1aed64bb4c2bbe676c201ee4da
SowmiyaThirugnanam <sowmiya.t2112@gmail.com>
2026-04-27 17:57:34 +0530

    New Students added for Banu and Ram

--- src/main/java/com/devops/student_api/StudentController.java
+++ src/main/java/com/devops/student_api/StudentController.java
@@ -1,8 +1,15 @@
 package com.devops.student_api;
 
-import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/students")
@@ -15,6 +22,7 @@ public class StudentController {
         studentList.add(new Student(2, "Rahul", "Java Backend"));
         studentList.add(new Student(3, "Priya", "Cloud Engineering"));
         studentList.add(new Student(4, "Banu", "Release Engineering"));
+        studentList.add(new Student(4, "Ram", "QA"));
     }
 
     @GetMapping
